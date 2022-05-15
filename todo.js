449aaf82a47e637076368fdd85ace968cbefd430
ujjwalsinngh <42001584+ujjwalsinngh@users.noreply.github.com>
2022-05-15 11:21:55 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,58 +2,75 @@ function submit(){
 
 	var task = document.getElementById("searchBox").value;
 
+	/to create new list on the basis of Id from HTML <div>/
 	let list = document.getElementById("taskList");
-	let li = document.createElement("li");
 
-	var x = document.createElement('input');
+	/to create list Item/
+	let listItem = document.createElement("li");
 
-	x.setAttribute("type", "radio");
-	x.setAttribute("value", task);
-	x.setAttribute("name", "listRadios");
 
-	var button = document.createElement("input");
+	/to generate manual Id in order to differentiate Ids of all list items/
+	const elementsForManualId = document.getElementById("taskList");
+	let initialLength = elementsForManualId.childNodes.length;
+	let manualId = task + initialLength;
 
-	button.setAttribute("type", "button");
-	button.id = 'remove';
-	button.value = '-';
-	button.name = 'remove';
 
-	// $("#delete").html('<input type="button" value="-" id="remove1" />');
-	
+	/current list item's id/
+	listItem.id = manualId;
 
-	li.append(x, x.value, button);
-	list.appendChild(li);
+
+	/to create a radio button at run-time (dynamically) & set it's properties/
+	var radioButton = document.createElement('input');
+	radioButton.setAttribute("type", "radio");                       /by using "setAttribute()" method /
+	radioButton.id = 'radio';                                        /by using function followed by property name/
+	radioButton.setAttribute("value", task);
+	radioButton.setAttribute("name", 'radioButton');
+
+	var currLabel = document.createElement("label");
+	currLabel.id = "label"
+	currLabel.name = manualId;
+	currLabel.value = task;
+
+
+	/to create delete button at run-time (dynamically) & set it's properties/
+	var deleteButton = document.createElement("input");
+	deleteButton.setAttribute("type", "button");
+	deleteButton.id = 'remove';
+	deleteButton.value = '-';
+	deleteButton.name = manualId;
+
+
+
+	/to append values to listItem/
+	listItem.append(radioButton, currLabel.value, deleteButton);
+
+	/to append list Item to list/
+	list.appendChild(listItem);
+
+	/to check a radio button corresponding to clicked list item/
+	listItem.onclick = function() {
+		radioButton.checked = true;
+		var rad = radioButton.value;
+		rad.style.color: "black";
+	}
+
+
+	deleteButton.onclick = function() {
+		/to remove <li> from the list on the basis of id (manual Id)/
+		document.getElementById(deleteButton.name).remove();
+
+		/to update "n tasks left" footer/
+		const elementsForLength = document.getElementById("taskList");
+		let len = elementsForLength.childNodes.length;
+		document.getElementById("length").innerHTML = len;
+	};
+
 
 	/to find the length of the list/
-	const elements = document.getElementById("taskList");
-	let len = elements.childNodes.length;
+	const elementsForLength = document.getElementById("taskList");
+	let len = elementsForLength.childNodes.length;
+	
 	document.getElementById("length").innerHTML = len;
 
-
-					// li.append(len);
-
-
-
-					// ${'#remove'}.hide();
-					// $("#taskList").hover(function(){
-					// 	$("#remove").show();
-					// });
-
-					// list.forEach((item, i) => {
-					// 	var listItem = document.createElement('label');
-					// 	listItem.innerHTML = item;
-					// 	document.body.appendChild(listItem);
-					// });
-
 }
-
-// $(#remove).on("click", "#remove1", function(){
-// 		document.getElementById("taskList").remove();
-// });
-
-$(document).ready(function(){
-	$(document).on("click", "", function(){
-		$(this).remove();
-	});
-});
 
