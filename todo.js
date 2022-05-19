da5969c28832a218bb26faf9b6c17e07c276744c
ujjwalsinngh <42001584+ujjwalsinngh@users.noreply.github.com>
2022-05-19 18:23:44 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,6 +2,9 @@ function submit(){
 
 	var task = document.getElementById("searchBox").value;
 
+	/to clear the input field after submit/
+	document.getElementById('searchBox').value = '';
+
 	/to create new list on the basis of Id from HTML <div>/
 	let list = document.getElementById("taskList");
 
@@ -10,9 +13,10 @@ function submit(){
 
 
 	/to generate manual Id in order to differentiate Ids of all list items/
-	const elementsForManualId = document.getElementById("taskList");
-	let initialLength = elementsForManualId.childNodes.length;
-	let manualId = task + initialLength;
+	// const elementsForManualId = document.getElementById("taskList");
+	// let initialLength = elementsForManualId.childNodes.length;
+	// let manualId = task + initialLength;
+	let manualId = Math.random();
 
 
 	/current list item's id/
@@ -37,7 +41,7 @@ function submit(){
 	var deleteButton = document.createElement("input");
 	deleteButton.setAttribute("type", "button");
 	deleteButton.id = 'remove';
-	deleteButton.value = '-';
+	deleteButton.value = '-'
 	deleteButton.name = manualId;
 
 
@@ -45,45 +49,61 @@ function submit(){
 	/to append values to listItem/
 	listItem.append(radioButton, currLabel.value, deleteButton);
 
+
 	/to append list Item to list/
 	list.appendChild(listItem);
 
+
+	/to store the current checked radio button/
+	var currChecked = manualId;
+
+	/to toggle for click on delete button /
+	var flag = false;
+
 	/to check & uncheck a radio button corresponding to clicked list item and update & reset it's style/
 	listItem.onclick = function() {
+
+		/if flag is false, means no click on any delete button/
+		if(!flag){
+			var list = document.getElementById('taskList').children;
 		
+			for(var i=0; i<list.length; i++){
 
-		var list = document.getElementById('taskList').children;
-		
-		for(var i=0; i<list.length; i++){
+				var item = list[i];
+				var rad = document.querySelectorAll('#radio');
+				var currRadio = rad[i];
 
-			var item = list[i];
-			var rad = document.querySelectorAll('#radio');
-			var currRad = rad[i];
+				if(item.id == listItem.id){
 
-			if(item.id == listItem.id){
-
-				currRad.checked = true;
-				currRad.parentElement.style.color = "white";
-				currRad.parentElement.style.backgroundColor = "black";
+					currRadio.checked = true;
+					currRadio.parentElement.style.color = "white";
+					currRadio.parentElement.style.backgroundColor = "black";
+					currChecked = radioButton;
 				
-			}else if(item.id != listItem.id && currRad.checked==true){
+				}else if(item.id != listItem.id){
 
-				currRad.checked = false; 
-				currRad.parentElement.style.color = "black";
-				currRad.parentElement.style.backgroundColor = "#ebedeb";
+					currRadio.checked = false; 
+					currRadio.parentElement.style.color = "black";
+					currRadio.parentElement.style.backgroundColor = "#ebedeb";
 
-			}else if(item.id != listItem.id && currRad.backgroundColor == 'black'){
-
-				currRad.parentElement.style.color = "black";
-				currRad.parentElement.style.backgroundColor = "#ebedeb";
+				}
 			}	
+		}else{   / if flag is true, means it came here after click on delete button and don't store current checked radio button coz it'll store the radio button of clicked list item as delete button is the part of clicked list item/
+			flag = false;
 		}
 	}
 
 
+	// / to delete clicked list item/
 	deleteButton.onclick = function() {
 		/to remove <li> from the list on the basis of id (manual Id)/
 		document.getElementById(deleteButton.name).remove();
+
+		/to check last checked radio button/
+		currChecked.checked = true;
+
+		/to toggle that delete button is clicked/
+		flag = true;
 
 		/to update "n tasks left" footer/
 		const elementsForLength = document.getElementById("taskList");
@@ -100,5 +120,4 @@ function submit(){
 	document.getElementById("length").innerHTML = len;
 
 }
-
 
